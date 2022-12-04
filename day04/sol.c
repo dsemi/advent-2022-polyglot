9fd59f4289d4e20774d219df7679f0af12b8eaed
Dan Seminara <semi225599@gmail.com>
2022-12-04 01:40:58 -0500

    Shorten

--- day04/sol.c
+++ day04/sol.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
   printf("Day 4: C\n");
   int p1 = 0, p2 = 0;
   int a0 = 0, a1 = 0, b0 = 0, b1 = 0;
   while (scanf("%d-%d,%d-%d", &a0, &a1, &b0, &b1) > 0) {
-    if (a0 <= b0 && a1 >= b1 || b0 <= a0 && b1 >= a1) {
-      p1++;
-    }
-    if (a0 <= b1 && b0 <= a1) {
-      p2++;
-    }
+    if (a0 <= b0 && a1 >= b1 || b0 <= a0 && b1 >= a1) p1++;
+    if (a0 <= b1 && b0 <= a1) p2++;
   }
   printf("Part 1: %20d\n", p1);
   printf("Part 2: %20d\n", p2);
