12efd07c80bfed2604576d282e9ee6ecd2941a9d
Dan Seminara <semi225599@gmail.com>
2022-12-18 13:35:39 -0500

    Shorten

--- day18/sol.kt
+++ day18/sol.kt
@@ -1,64 +1,46 @@
-import java.util.ArrayDeque
-
 data class Pt(val x: Int, val y: Int, val z: Int) {
     fun adj(): List<Pt> {
         return listOf(Pt(x+1, y, z), Pt(x-1, y, z),
                       Pt(x, y+1, z), Pt(x, y-1, z),
                       Pt(x, y, z+1), Pt(x, y, z-1))
     }
+
+    fun min(o: Pt): Pt {
+        return Pt(minOf(x, o.x), minOf(y, o.y), minOf(z, o.z))
+    }
+
+    fun max(o: Pt): Pt {
+        return Pt(maxOf(x, o.x), maxOf(y, o.y), maxOf(z, o.z))
+    }
+
+    operator fun plus(o: Pt): Pt {
+        return Pt(x + o.x, y + o.y, z + o.z)
+    }
 }
 
 fun main() {
     println("Day 18: Kotlin")
-    var space = mutableSetOf<Pt>()
-    var p1 = 0
-    var p2 = 0
-    for (line in generateSequence(::readLine)) {
-        val pts = line.split(",")
-        val c = Pt(pts[0].toInt(), pts[1].toInt(), pts[2].toInt())
-        p1 += 6
-        for (c2 in c.adj()) {
-            if (space.contains(c2)) {
-                p1 -= 2
-            }
-        }
-        space.add(c)
-    }
+    val lava = generateSequence(::readLine).map {
+        val pts = it.split(",")
+        Pt(pts[0].toInt(), pts[1].toInt(), pts[2].toInt())
+    }.toSet()
+    val p1 = lava.flatMap { it.adj() }.count { !lava.contains(it) }
     println("Part 1: %20d".format(p1))
-    var minX = Int.MAX_VALUE
-    var maxX = Int.MIN_VALUE
-    var minY = Int.MAX_VALUE
-    var maxY = Int.MIN_VALUE
-    var minZ = Int.MAX_VALUE
-    var maxZ = Int.MIN_VALUE
-    for (c in space) {
-        minX = minOf(minX, c.x-1)
-        maxX = maxOf(maxX, c.x+1)
-        minY = minOf(minY, c.y-1)
-        maxY = maxOf(maxY, c.y+1)
-        minZ = minOf(minZ, c.z-1)
-        maxZ = maxOf(maxZ, c.z+1)
+    var lo = Pt(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE)
+    var hi = Pt(Int.MIN_VALUE, Int.MIN_VALUE, Int.MIN_VALUE)
+    for (c in lava) {
+        lo = lo.min(c + Pt(-1, -1, -1))
+        hi = hi.max(c + Pt(1, 1, 1))
     }
-    val topLeft = Pt(minX, minY, minZ)
-    val botRight = Pt(maxX, maxY, maxZ)
-    var visited = mutableSetOf(topLeft, botRight)
-    var frontier = ArrayDeque<Pt>()
-    frontier.add(topLeft)
-    frontier.add(botRight)
+    var air = mutableSetOf(lo, hi)
+    var frontier = ArrayDeque<Pt>(air)
     while (!frontier.isEmpty()) {
-        val pt = frontier.remove()
-        for (pt2 in pt.adj()) {
-            if (pt2.x < minX || pt2.x > maxX || pt2.y < minY || pt2.y > maxY || pt2.z < minZ || pt2.z > maxZ) {
-                continue
-            }
-            if (space.contains(pt2)) {
-                p2 += 1
-                continue
-            }
-            if (visited.add(pt2)) {
-                frontier.add(pt2)
-            }
-        }
+        frontier.addAll(
+            frontier.removeFirst().adj().filter {
+                (lo.x..hi.x).contains(it.x) && (lo.y..hi.y).contains(it.y) && (lo.z..hi.z).contains(it.z)
+                && !lava.contains(it) && air.add(it)
+            })
     }
+    val p2 = lava.flatMap { it.adj() }.count { air.contains(it) }
     println("Part 2: %20d".format(p2))
 }
