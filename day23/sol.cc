177cb5bf6e4dcca4e53002778d007f676a55297d
Dan Seminara <semi225599@gmail.com>
2022-12-30 21:54:14 -0500

    Remove using namespace std

--- day23/sol.cc
+++ day23/sol.cc
@@ -7,16 +7,14 @@
 #include <set>
 #include <vector>
 
-using namespace std;
-
-constexpr int MIN_INT = numeric_limits<int>::min();
-constexpr int MAX_INT = numeric_limits<int>::max();
+constexpr int MIN_INT = std::numeric_limits<int>::min();
+constexpr int MAX_INT = std::numeric_limits<int>::max();
 
 constexpr int SZ = 2500;
 
-constexpr array<int, 8> DIRS{ {-SZ - 1, -SZ, -SZ + 1, -1, 1, SZ - 1, SZ, SZ + 1}};
+constexpr std::array<int, 8> DIRS{ {-SZ - 1, -SZ, -SZ + 1, -1, 1, SZ - 1, SZ, SZ + 1}};
 
-constexpr array<array<int, 3>, 4> PROP_DIRS{ {
+constexpr std::array<std::array<int, 3>, 4> PROP_DIRS{ {
   {-SZ - 1, -SZ, -SZ + 1},
   {SZ - 1,   SZ,  SZ + 1},
   {-SZ - 1, -1,  SZ - 1},
@@ -28,10 +26,10 @@ struct Elf {
   int prop;
 };
 
-vector<Elf*> parse() {
-  vector<Elf*> elves{};
+std::vector<Elf*> parse() {
+  std::vector<Elf*> elves{};
   int y = 0;
-  for (string line; getline(cin, line); y++) {
+  for (std::string line; getline(std::cin, line); y++) {
     for (int x = 0; x < line.length(); x++) {
       if (line[x] == '#') {
         elves.push_back(new Elf {.pos = (y + SZ / 2)*SZ + x + SZ/2, .prop = MIN_INT });
@@ -41,11 +39,11 @@ vector<Elf*> parse() {
   return elves;
 }
 
-bool step(vector<Elf*>& elves, vector<Elf*>& grid, vector<int>& props, int dir) {
+bool step(std::vector<Elf*>& elves, std::vector<Elf*>& grid, std::vector<int>& props, int dir) {
   for (Elf* elf : elves) {
-    if (any_of(DIRS.cbegin(), DIRS.cend(), [&elf, &grid](int d){ return grid[elf->pos+d] != NULL; })) {
+    if (std::any_of(DIRS.cbegin(), DIRS.cend(), [&elf, &grid](int d){ return grid[elf->pos+d] != NULL; })) {
       for (int i = 0; i < 4; i++) {
-        array<int, 3> prop = PROP_DIRS[(dir+i) % 4];
+        std::array<int, 3> prop = PROP_DIRS[(dir+i) % 4];
         if (grid[elf->pos + prop[0]] == NULL
             && grid[elf->pos + prop[1]] == NULL
             && grid[elf->pos + prop[2]] == NULL) {
@@ -73,13 +71,13 @@ bool step(vector<Elf*>& elves, vector<Elf*>& grid, vector<int>& props, int dir) 
 }
 
 int main() {
-  cout << "Day 23: C++" << endl;
-  vector<Elf*> elves = parse();
-  vector<Elf*> grid(SZ*SZ);
+  std::cout << "Day 23: C++" << std::endl;
+  std::vector<Elf*> elves = parse();
+  std::vector<Elf*> grid(SZ*SZ);
   for (Elf* elf : elves) {
     grid[elf->pos] = elf;
   }
-  vector<int> props(SZ*SZ);
+  std::vector<int> props(SZ*SZ);
   int i = 0;
   for (; i < 10; i++) {
     step(elves, grid, props, i % 4);
@@ -91,15 +89,15 @@ int main() {
   for (Elf* elf : elves) {
     int x = elf->pos % SZ;
     int y = elf->pos / SZ;
-    min_x = min(min_x, x);
-    min_y = min(min_y, y);
-    max_x = max(max_x, x + 1);
-    max_y = max(max_y, y + 1);
+    min_x = std::min(min_x, x);
+    min_y = std::min(min_y, y);
+    max_x = std::max(max_x, x + 1);
+    max_y = std::max(max_y, y + 1);
   }
-  cout << "Part 1: " << setw(20) << (max_x - min_x)*(max_y - min_y) - elves.size() << endl;
+  std::cout << "Part 1: " << std::setw(20) << (max_x - min_x)*(max_y - min_y) - elves.size() << std::endl;
   for (;; i++) {
     if (!step(elves, grid, props, i % 4)) break;
   }
-  cout << "Part 2: " << setw(20) << i + 1 << endl;
+  std::cout << "Part 2: " << std::setw(20) << i + 1 << std::endl;
   return 0;
 }
